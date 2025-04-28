4957ecaabe3cc22cafce5a4674c333a246278d1f
ChenXin <ysuchx@sina.com>
2025-04-28 19:44:32 +0800

    Add HDCoverage.h

---
+++ HDCoverage/HDCoverage.h
@@ -0,0 +1,13 @@
+//
+//  HDCoverage.h
+//  Stock
+//
+//  Created by chenxin on 2025/4/28.
+//  Copyright © 2025 com.tigerbrokers. All rights reserved.
+//
+
+#ifndef HDCoverage_h
+#define HDCoverage_h
+
+
+#endif /* HDCoverage_h */
